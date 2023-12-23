9b2dc138553fa13fe12d01ae5c43097d2f19c803
Natalie Fortes <nf458c@gmail.com>
2023-12-23 00:38:24 -0500

    Refactor code, connect routes

---
+++ routes/contact.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+const router = Router();
+// import 
+
+export default router;
---
+++ routes/pages.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+const router = Router();
+// import 
+
+router.route('/')
+    .get(function (req, res) {
+        res.render('home', { pageTitle: 'Lili\'s Cafe' });
+    });
+
+router.route('/my-interests')
+    .get(function (req, res) {
+        res.render('my-interests', { pageTitle: 'My Interests' });
+    })
+
+router.route('/skills-and-experience')
+    .get(function (req, res) {
+        res.render('skills-and-experience', { pageTitle: 'Skills and Experience' });
+    })
+
+export default router;
